03735e396633c695e731bc4aebdc95db0a67008c
jpabl@0s1n <jpablo_166@hotmail.com>
2023-09-02 23:07:59 -0500

    feat: Initial

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+	return (
+		<div className='container'>
+			<h1>Quiz App</h1>
+			<a href='/game' className='btn'>
+				Start Game
+			</a>
+			<a href='/highScores' className='btn'>
+				High Scores
+			</a>
+		</div>
+	);
+}
